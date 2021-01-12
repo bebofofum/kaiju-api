d796f5a3038bad4c2a801e9d79adb6fc059330cc
bebofofum <bri.boehm@gmail.com>
2021-01-12 12:59:05 -0500

    Added nested routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :sightings
-  resources :titans
+  resources :titans do
+    resources :sightings, only: [:new, :index, :show]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
